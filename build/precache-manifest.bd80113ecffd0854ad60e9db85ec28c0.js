8956b955d53945c109f35e9376f3c2a57ae2bf74
wlqlsg <1547280633@qq.com>
2020-10-03 11:02:11 +0800

    add

---
+++ build/precache-manifest.bd80113ecffd0854ad60e9db85ec28c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286b39ad052e260da3d081b60a9d3e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67d1222f338ac286aec",
+    "url": "/static/js/main.97839896.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
